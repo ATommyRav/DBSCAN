27c76172170571b3818f3ed8f60f595c3e294dd3
Papillon02 <mahalianaranaivoson004@gmail.com>
2025-10-06 23:47:25 +0300

    Update Etendre_cluster.py

--- Projet_dbscan/dbscan/Etendre_cluster.py
+++ Projet_dbscan/dbscan/Etendre_cluster.py
@@ -1,4 +1,4 @@
-from Voisinage import voisinage
+from Voisinage import Voisinage
 def etendre_cluster(X, groupes, P_indice, PtsVoisins, k, eps, m):
 """ X : c'est l'ensemble des points [X1,X2,...,Xn] avec X1 appartenant a R^d
     groupes (list[int]) : pour identifier chaque point (si =0 : non visité, si =-1: bruit, si  >0 : cluster), cette marquage se fait dans main_dbscan.
@@ -7,13 +7,13 @@ def etendre_cluster(X, groupes, P_indice, PtsVoisins, k, eps, m):
     k : Numéro du cluster actuel
     eps : Rayon du voisinage
     m :	Nombre minimum de points pour être un point cœur """
-    groupes[P_indice] = C # On marque le point de départ P comme appartenant au cluster numéro C (avec P un point coeur qui sera verifier dans main_dbscan)
+    groupes[P_indice] = k # On marque le point de départ P comme appartenant au cluster numéro C (avec P un point coeur qui sera verifier dans main_dbscan)
     i = 0
     while i < len(PtsVoisins): #On traite les voisins de P
       Pv_indice = PtsVoisins[i]
       if groupes[Pv_indice] == 0: # Si le voisins de P est marque non visite auparavant alors on le met dans le meme cluster que P 
          groupes[Pv_indice] = k
-         PtsVoisinsP = voisinage(X, X[Pp_index], eps) #on cherche les voisins de voisin de P
+         PtsVoisinsP = Voisinage(X, X[Pp_index], eps) #on cherche les voisins de voisin de P
          if len(PtsVoisinsP) >= m : #Si ce voisin est un point coeur alors ses voisins s'ajoutent aussi au cluster
             for v in PtsVoisinsP:
                if v not in PtsVoisins:
