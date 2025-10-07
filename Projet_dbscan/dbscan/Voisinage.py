5552927caa8f82f5971b3a4cfe44f8e031eb1400
RAVELOJOELITAFIKA Tommy Alan <tommalan2002@gmail.com>
2025-10-07 08:15:29 +0300

    Update Voisinage.py
    Changement de boule qui etait l'ensemble de point, en ensemble d'indice de points pour être compatible avec la fonction etendre_cluster

--- Projet_dbscan/dbscan/Voisinage.py
+++ Projet_dbscan/dbscan/Voisinage.py
@@ -1,7 +1,7 @@
 from distance import distance_euclidienne as distance
 def Voisinage(X, point, epsilon):# X est l'ensemble des donnée, point est l'element dont on cherche la boule ouvret centrée en ce point, epsilon est e rayon de la boule
     boule = [] #initiation de la boule ouverte
-    for y in X:
-        if (distance(point,y)) < epsilon:
-            boule.append(y) # ajoute a boule les element proche de points à un distance inferieur de epsilon
+    for i in range(len(X)):
+        if (distance(point,X[i])) < epsilon:
+            boule.append(i) # ajoute a boule les element proche de points à un distance inferieur de epsilon
     return boule   #renvoie la boule ouverte centrée en pint
